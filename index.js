a0af17b51a863307ab14d38a22088331c08d7574
berg <joselindemberg5@protonmail.com>
2021-03-09 09:34:02 -0300

    Train EJS and, create new files

--- index.js
+++ index.js
@@ -3,9 +3,25 @@ const app = express();
 
 app.set('view engine', 'ejs');
 //estou dizendo para o express que minha view engine é o ejs
+app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-	res.send('welcome to my page!');
+app.get('/:name/:lang', (req, res) => {
+	const name = req.params.name;
+	const lang = req.params.lang;
+	const showMsg = true;
+	const products = [
+		{name: 'doritos', price: 3.14},
+		{name: 'water', price: 5},
+		{name: 'milk', price: 5.5}
+	]
+
+	res.render('index',{
+		name: name,
+		lang: lang,
+		subs: 19,
+		msg: showMsg,
+		products: products
+	});
 });
 
 app.listen(8080,() => {
