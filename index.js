ca46a2b67b0f41f0d5e42f2e5ca9a345443e6d97
berg <joselindemberg5@protonmail.com>
2021-04-01 08:25:36 -0300

    Add /salvarpergunta route and sequelize

--- index.js
+++ index.js
@@ -5,12 +5,23 @@ app.set('view engine', 'ejs');
 //estou dizendo para o express que minha view engine é o ejs
 app.use(express.static('public'));
 
+app.use(express.urlencoded({extended: true}));
+//decodifica os dados do form para ser usado no js
+app.use(express.json());
+//permite a leitura de json
+
 app.get('/', (req, res) => {
 	res.render('index');
 });
 
 app.get('/perguntar', (req, res) => {
-	res.render('perguntar')
+	res.render('perguntar');
+});
+
+app.post('/salvarpergunta', (req, res) => {
+	const titulo = req.body.titulo;
+	const descricao = req.body.descricao;
+	res.send(`form received! title:${titulo} description:${descricao}`);
 });
 
 app.listen(8080,() => {
